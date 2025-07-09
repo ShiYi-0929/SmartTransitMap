88de433e50b33a3b72b5752573bbcac585aa0465
cus158 <168059543+cus158@users.noreply.github.com>
2025-07-09 16:42:38 +0800

    Update index.js
    加上路面识别后的

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,46 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Face from '../pages/Face.vue'
-import Road from '../pages/Road.vue'
-import Log from '../pages/Log.vue'
-import Traffic from '../pages/Traffic.vue'
-import AllViews from '../pages/AllViews.vue'
-import UserManagement from '../pages/UserManagement.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../pages/Home.vue"
+import Face from "../pages/Face.vue"
+import Road from "../pages/Road.vue"
+import Log from "../pages/Log.vue"
+import Traffic from "../pages/Traffic.vue"
+import AllViews from "../pages/AllViews.vue"
+import UserManagement from "../pages/UserManagement.vue"
 
 const routes = [
-  { path: '/', component: AllViews },
-  { path: '/home', component: Home },
-  { path: '/face', component: Face },
-  { path: '/road', component: Road },
-  { path: '/log', component: Log },
-  { path: '/traffic', component: Traffic },
-  { path: '/user-management', component: UserManagement }
+  { path: "/", component: AllViews },
+  { path: "/home", component: Home },
+  { path: "/face", component: Face },
+  { path: "/road", component: Road },
+  { path: "/log", component: Log },
+  { path: "/traffic", component: Traffic },
+  { path: "/user-management", component: UserManagement },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = !!localStorage.getItem('user-token');
-
-  if (to.path === '/' && isAuthenticated) {
-    return next('/home');
+  const isAuthenticated = !!localStorage.getItem("user-token")
+  if (to.path === "/" && isAuthenticated) {
+    return next("/home")
   }
-
-  const publicPages = ['/'];
-  const authRequired = !publicPages.includes(to.path);
-
+  const publicPages = ["/"]
+  const authRequired = !publicPages.includes(to.path)
   if (authRequired && !isAuthenticated) {
-    if (to.path === '/home') {
-      return next(false); // 对首页静默取消导航
+    if (to.path === "/home") {
+      return next(false) // 对首页静默取消导航
     }
-    alert('请先登录');
-    return next(false); // 对其他页面提示并取消导航
+    alert("请先登录")
+    return next(false) // 对其他页面提示并取消导航
   }
+  next()
+})
 
-  next();
-});
+export default router
+
 
 export default router 
