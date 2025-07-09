dd94e49d7db2b99c9caa863c695bdb19474b5c5a
cus158 <168059543+cus158@users.noreply.github.com>
2025-07-09 16:33:55 +0800

    Add files via upload

---
+++ frontend/src/pages/RoadDetectionPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppLayout>
+    <RoadDamageDetection />
+  </AppLayout>
+</template>
+
+<script>
+import AppLayout from "@/components/AppLayout.vue";
+import RoadDamageDetection from "@/components/RoadDamageDetection.vue";
+
+export default {
+  name: "RoadDetectionPage",
+  components: {
+    AppLayout,
+    RoadDamageDetection,
+  },
+};
+</script>
