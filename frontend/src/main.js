b05ced6987677324a731b8cf4958cd69fdd97e22
cus158 <168059543+cus158@users.noreply.github.com>
2025-07-09 16:51:14 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from "./store";
+import "./assets/css/tailwind.css";
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
